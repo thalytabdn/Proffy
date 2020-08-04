03154a30d1d11c2f8011f01529b8ce872df7841d
thalytabdn <thalyta.nascimento@ccc.ufcg.edu.br>
2020-08-04 18:33:01 -0300

    interface

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import "./styles.css";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars2.githubusercontent.com/u/2254731?s=400&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4"
+          alt="Diego Fernandes"
+        />
+        <div>
+          <strong>Diogo Fernandes</strong>
+          <span>Quimica</span>
+        </div>
+      </header>
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br/><br/>
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experências.
+        Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 35,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em Contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./styles.css";
+import PageHeader from "../../components/PageHeader/";
+
+export default function TeacherForm() {
+  return (
+    <div className="container" id="page-teacher-form">
+      <PageHeader title="Que incrível que você quer dar aulas" />
+    </div>
+  );
+}
